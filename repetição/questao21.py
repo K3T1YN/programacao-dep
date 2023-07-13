7ce79f9157bd69765919ac9cc8581dbd85968fae
K3T1YN <99234014+K3T1YN@users.noreply.github.com>
2023-07-13 11:30:05 -0300

    13/07

---
+++ repetição/questao21.py
@@ -0,0 +1,6 @@
+#10 numeros
+cont = 0 
+for n in range(1,3):
+    n = int(input("Digite um número:"))
+    cont = cont + n
+print(cont)
--- repetição/teste.py
+++ repetição/teste.py
@@ -3,3 +3,8 @@ for name in "ketlyn":
 
 for i in range(1,11):
     print(i)
+
+n = str(input("Digite uma palavra:"))
+repeticao = int(input("Digiteb a quantidade de vezes você quer que repita:"))
+for repeticao in range(repeticao):
+    print(n)
